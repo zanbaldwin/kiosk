379660fa6ca8a9393918cbd963a53e33139be7e3
Zander Baldwin <mynameiszanders@gmail.com>
2013-04-12 21:06:41 +0100

    Overhaul the script. URL parsing; re-arrangement of commands, use Here Docs with echo statements, etc.

--- kiosk.sh
+++ kiosk.sh
@@ -1,79 +1,126 @@
+#
+# KIOSK SETUP
+# ===========
+#
+# Setup a Web Kiosk on a Debian-based system.
+# This script has been customised for use on a fresh installation of Raspbian
+# Wheezy, so many unnecessary packages may still remain on your system causing complications with the current setup if
+# you use another distribution.
+#
+# @author       Zander Baldwin <mynameiszanders@gmail.com>
+# @license      MIT/X11 <http://j.mp/mit-license>
+# @copyright    2013 Zander Baldwin
+#
+
 TITLE="Kiosk Setup"
+
+kiosk_install() {
+
+    # PACKAGES.
+
+}
 
 setup_kiosk() {
     INSTALLCERT="$1"
-
-    PROTOCOL="http"
-    DOMAIN=$(whiptail --inputbox "Please enter the domain of the URL you wish the Kiosk to point to (for example, \"example.com\"):" 8 78 --title "$TITLE" 3>&1 1>&2 2>&3 | sed "s#/*$##g")
-    URL=$(whiptail --inputbox "Please enter the path of the URL you wish the Kiosk to point to (for example, \"/kiosk/home.php\"):" 8 78 --title "$TITLE" 3>&1 1>&2 2>&3 | sed "s#^/*##g")
+    # Ask what URL the Web Kiosk should load when it starts.
+    URL=$(whiptail --inputbox "Please enter the path of the full URL you wish the Kiosk to point to (for example, \"http://example.com/kiosk/home.php\"):" --title "$TITLE" 10 78 3>&1 1>&2 2>&3)
+    # Strip out "://" and everything after it.
+    URL_PROTOCOL=${URL%%://*}
+    # Strip out "://" and everything before it, then strip out the first "/" and
+    # everything after it.
+    URL_DOMAIN=${URL#*://}
+    URL_DOMAIN=${URL_DOMAIN%%/*}
+    # Strip out everything the first "/" and everything after it that occurs
+    # after "://".
+    URL_PATH=${URL#*://*/}
+    # If no path was specified in the URL, $URL_PATH will not have matched and
+    # returned the entire URL. Set an empty string if this is the case.
+    if [ $URL = $URL_PATH ]; then
+        URL_PATH=""
+    fi
     CACN=""
     if [ $INSTALLCERT -eq 1 ]; then
-        PROTOCOL="https"
-        CACN=$(whiptail --inputbox "Please enter the exact Common Name of the Certification Authority that signs the PKCS#12 certificate for this Kiosk (for example, \"My Company Kiosk CA\"):" 8 78 --title "$TITLE" 3>&1 1>&2 2>&3)
+        URL_PROTOCOL="https"
+        CACN=$(whiptail --inputbox "Please enter the exact Common Name of the Certification Authority that signs the PKCS#12 certificate for this Kiosk (for example, \"My Company Kiosk CA\"):" 10 78 --title "$TITLE" 3>&1 1>&2 2>&3)
     fi
-
-    # PACKAGES
-    # ========
 
     # Update package repository lists.
     apt-get update
     # Remove unused packages.
     aptitude purge -y lxappearance lxde lxde-common lxde-core lxde-icon-theme lxinput lxmenu-data lxpanel lxpolkit lxrandr lxsession lxsession-edit lxshortcut lxtask lxterminal xinit xserver-xorg lightdm scratch midori desktop-base desktop-file-utils gnome-icon-theme gnome-themes-standard leafpad menu-xdg omxplayer xarchiver zenity tk8.5 pcmanfm blt idle idle3 python-tk python3-tk dillo openbox gvfs gvfs-backends gvfs-common gvfs-daemons gvfs-fuse gvfs-libs pistore obconf
-    # Install required packages.
-    aptitude install --without-recommends -y xorg slim matchbox-window-manager chromium-browser rsync openssh-server libnss3-tools
     # Upgrade existing packages.
     apt-get upgrade -y
+    # Install required packages.
+    aptitude install --without-recommends -y xorg slim matchbox-window-manager chromium-browser rsync openssh-server libnss3-tools
 
     # PACKAGE CONFIGURATION
-    # =====================
 
     # Configuration for Chromium
     mkdir -p /etc/chromium/policies/managed
     touch /etc/chromium/policies/managed/kiosk.json
-    echo "{
-    \"AutoSelectCertificateForUrls\": [
-        \"{\\\"pattern\\\":\\\"https://$DOMAIN\\\",\\\"filter\\\": {\\\"ISSUER\\\":{\\\"CN\\\":\\\"$CACN\\\"}}}\"
+    echo <<\EOF  > /etc/chromium/policies/managed/nosco-systems.json
+{
+    "AutoSelectCertificateForUrls": [
+        "{\"pattern\":\"$URL_PROTOCOL://$URL_DOMAIN\",\"filter\": {\"ISSUER\":{\"CN\":\"$CACN\"}}}"
     ],
-    \"HomepageLocation\": \"$PROTOCOL://$DOMAIN/$URL\"
-}" > /etc/chromium/policies/managed/kiosk.json
-
+    "HomepageLocation": "$URL_PROTOCOL://$URL_DOMAIN/$URL_PATH"
+}
+EOF
     # Configuration for Slim
     echo "default_user kiosk" >> /etc/slim.conf
     echo "auto_login yes" >> /etc/slim.conf
 
-    # SYSTEM USER SETUP
-    # =================
+    # USERS
 
+    # Setup a system user to run the Web Kiosk as.
     useradd -m -U kiosk
     chmod -R a+r /home/kiosk
     touch /home/kiosk/.xsession
-    echo "xset s off
+    echo <<\EOF > /home/kiosk/.xsession
+xset s off
 xset -dpms
 matchbox-window-manager &
 while true; do
     rsync -qr --delete --exclude='.Xauthority' /opt/kiosk/ \$HOME/
-    chromium-browser --kiosk $PROTOCOL://$DOMAIN/$URL
-done" > /home/kiosk/.xsession
+    chromium-browser --kiosk
+done
+EOF
     chmod a+x /home/kiosk/.xsession
     cp /home/kiosk/.xsession /home/kiosk/.xinitrc
 
-    # ADD CLIENT CERTIFICATES
-    # =======================
-
+    # Set the directory that Chromium uses to look for client certificates.
     CERTSTORE="/home/kiosk/.pki/nssdb"
+    # Make the directory.
     mkdir -p $CERTSTORE
+    # Pass ownership to the Web Kiosk user.
     chown -R kiosk:kiosk $CERTSTORE
+    # Add the Web Kiosk user, create the certificate database.
     sudo -u kiosk certutil -d $CERTSTORE -N
+    # If client certificate authentication was selected, install the PKCS#12
+    # certificate as the Web Kiosk user to the certificate database.
     if [ $INSTALLCERT -eq 1 ]; then
         sudo -u kiosk pk12util -d "sql:$CERTSTORE" -i "/tmp/kiosk.p12" -W ""
     fi
 
-    # CREATE CUSTOM SKEL DIRECTORY FOR SYSTEM USER
-    # ============================================
-
+    # Create a blueprint of the Web Kiosk's user directory in /opt. Using rsync,
+    # the Web Kiosk's user directory is temporary and only lasts the duration of
+    # the session.
     cp -r /home/kiosk /opt/
 
+    # You may wish to perform additional actions as the root user before booting into the Web Kiosk.
+    whiptail --yesno "Would you like to reboot into the Web Kiosk now?" 20 60 2
+    if [ $? -eq 0 ]; then
+        sync
+        reboot
+    else
+        exit 0
+    fi
+
 }
+
+#
+# BEGIN SCRIPT HERE.
+#
 
 if [ $(id -u) -ne 0 ]; then
   printf "This script must be run as root. Try prefixing with 'sudo'.\n"
@@ -87,12 +134,13 @@ if [ ${#WHIPTAILPATH} -eq 0 ]; then
 fi
 
 # Ask whether to setup automatic login via client certificates.
-whiptail --title $TITLE --yesno "Would you like to setup automatic login, via certificates, or manual login, via username and password?" 20 60 2 --yes-button "Automatic" --no-button "Manual"
+#AUTOLOGIN=$(whiptail --title $TITLE --yesno "Would you like to setup automatic login, via certificates, or manual login, via username and password?" 20 60 2 --yes-button "Automatic" --no-button "Manual")
+whiptail --yesno "Would you like to setup automatic login, via certificates, or manual login, via username and password?" 10 60 2 --yes-button "Automatic" --no-button "Manual"
 AUTOLOGIN=$?
 
-if [ $AUTOLOGIN -eq 0 ]; then
+if [ "$AUTOLOGIN" -eq 0 ]; then
     # Ask which Loan Station we are setting up a certificate for.
-    KIOSKP12=$(whiptail --inputbox "Please enter the URL of the Kiosk PKCS#12 certificate to be installed:" 8 78 --title "$TITLE" 3>&1 1>&2 2>&3)
+    KIOSKP12=$(whiptail --title "$TITLE" --inputbox "Please enter the URL of the Kiosk PKCS#12 certificate to be installed:" 10 78 3>&1 1>&2 2>&3)
     IDENTERED=$?
     if [ $IDENTERED = 0 ]; then
         wget "$KIOSKP12" --output-document="/tmp/kiosk.p12"
@@ -102,15 +150,18 @@ if [ $AUTOLOGIN -eq 0 ]; then
             if [ $P12SIZE -ne 0 ]; then
                 setup_kiosk 1
             else
-                whiptail --title "$TITLE" --msgbox "The certificate downloaded for the Kiosk ID you specified is not valid. Setup has been cancelled." 8 78
+                whiptail --title "$TITLE" --msgbox "The certificate downloaded for the Kiosk ID you specified is not valid. Setup has been cancelled." 10 78
             fi
         else
-            whiptail --title "$TITLE" --msgbox "The certificate for the Kiosk ID you specified could not be downloaded. Setup has been cancelled." 8 78
+            whiptail --title "$TITLE" --msgbox "The certificate for the Kiosk ID you specified could not be downloaded. Setup has been cancelled." 10 78
         fi
     else
-        whiptail --title "$TITLE" --msgbox "Setup has been cancelled. This is not yet a Kiosk, please run this setup again to proceed." 8 78
+        whiptail --title "$TITLE" --msgbox "Setup has been cancelled. This is not yet a Kiosk, please run this setup again to proceed." 10 78
     fi
 else
-    whiptail --title "$TITLE" --msgbox "You may have to manually login to the Kiosk each time you turn the machine on." 8 78
+    whiptail --title "$TITLE" --msgbox "You may have to manually login to the Kiosk each time you turn the machine on." 10 78
     setup_kiosk 0
 fi
+
+# If you get here it means this script did not complete correctly. Exit with an error status.
+exit 1
